9d47e26cd7899462e617a2727942e2608e713fcd
lwyso <lukasz-wysocki18@wp.pl>
2019-09-18 15:59:22 +0200

    remove body-parser

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const db = require('./app');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(express.json());
 
 app.get('/todos', (req, res) => {
     const sql = 'select * from todo';
@@ -15,82 +11,78 @@ app.get('/todos', (req, res) => {
             res.status(500).send({ error: 'Something failed!' })
         }
 
-        if (!results.length) {
+        if (results.length === 0) {
             res.status(404).send({ error: 'Todos not found' });
         } else {
-            res.status(200);
-            res.json(results)
+            res.status(200).json(results);
         }
     });
 });
 
 app.post('/todos', (req, res) => {
     const title = req.body.title;
-    const sql = `insert into todo (title) values ('${title}')`;
+    const sql = 'insert into todo (title) values (?)';
 
-    db.query(sql, (err, results) => {
+    db.query(sql, [title], (err, results) => {
         if (err) {
-            console.log(sql)
             res.status(500).send({ error: 'Something failed!' })
         }
 
-        if (!title || !title.length) {
+        if (!title || title.length === 0) {
             res.status(400).send({ error: 'Wrong data' })
         } else {
-            res.status(201);
-            res.json(results);
+            res.status(201).json(results);
         }
     });
 });
 
 app.get('/todo/:id', (req, res) => {
     const id = req.params.id;
-    const sql = `select * from todo where id=${id}`;
+    const sql = 'select * from todo where id=?';
 
-    db.query(sql, (err, results) => {
+    db.query(sql, [id], (err, results) => {
         if (err) {
             res.status(500).send({ error: 'Something failed!' })
         }
 
-        if (!results.length) {
+        if (results.length === 0) {
             res.status(404).send({ error: 'Todo not found' })
         } else {
-            res.status(200);
-            res.json(results);
+            res.status(200).json(results[0]);
         }
     });
 });
 
 app.put('/todo/:id', (req, res) => {
     const id = req.params.id;
-    const sql = `update todo set flag=1 where id=${id}`;
+    const sql = 'update todo set flag=1 where id=?';
 
-    db.query(sql, (err, results) => {
+    db.query(sql, [id], (err, results) => {
         if (err) {
             res.status(500).send({ error: 'Something failed' });
         }
 
-        if (!results.affectedRows) {
+        if (results.affectedRows === 0) {
             res.status(404).send({ error: 'Todo not found' })
         } else {
-            res.status(204);
+            res.status(204).end();
         }
     });
 });
 
 app.delete('/todo/:id', (req, res) => {
     const id = req.params.id;
-    const sql = `delete from todo where id=${id}`;
+    const sql = 'delete from todo where id=?';
 
-    db.query(sql, (err, results) => {
+    db.query(sql, [id], (err, results) => {
         if (err) {
             res.status(500).send({ error: 'Something failed!' })
         }
 
-        if (!results.affectedRows) {
+        if (results.affectedRows === 0) {
             res.status(404).send({ error: 'Todo not found' })
         } else {
-            res.status(204);
+            res.status(204).end();
         }
     });
 });
