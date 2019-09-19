80702baac842547b8f02e31d6376693ea1b027c3
lwyso <lukasz-wysocki18@wp.pl>
2019-09-19 16:45:47 +0200

    fetch todos

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.static('public'))
+
 app.get('/todos', (req, res) => {
     const sql = 'select * from todo';
     db.query(sql, (err, results) => {
